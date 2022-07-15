eb8e8f7a196bad91ab0fb01574ae4eca45b13cb7
MrGians <gianlumura97@gmail.com>
2022-07-15 15:12:25 +0200

    Contact filter - Milestone 4 Done

--- js/script.js
+++ js/script.js
@@ -1,25 +1,3 @@
-// Milestone 1
-// Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e dall’interlocutore (bianco) assegnando due classi CSS diverse
-// Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto
-
-// Milestone 2
-// Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione
-// Click sul contatto mostra la conversazione del contatto cliccato
-
-// Milestone 3
-// Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando “enter” il testo viene aggiunto al thread sopra, come messaggio verde
-// Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
-
-// Milestone 4
-// Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo “mar” rimangono solo Marco e Martina)
-// Trovate in allegato gli screenshot dell'app e una base di partenza coi dati utili per la milestone di oggi.
-
-// Consigli Utili:
-// non dimentichiamo di fare analisi sia per la struttura dati che per la grafica
-// procediamo a blocchettoni per evitare di avere poi problemi col CSS in fase avanzata
-// Cerchiamo di rispettare tutti i principi e le best practices viste finora (nomi di variabili e classi, centralizzazione ecc)
-// Buon lavoro e soprattutto buon divertimento!!
-
 // Vue JS
 const boolzapp = new Vue({
   el: "#boolzapp",
@@ -28,6 +6,7 @@ const boolzapp = new Vue({
       name: "Nome Utente",
       avatar: "_io",
     },
+    filteredQuery: "",
     newMessage: "",
     currentContact: undefined,
     contacts: [
@@ -152,6 +131,16 @@ const boolzapp = new Vue({
         status: "received",
       };
       this.currentContact.messages.push(newMessage);
+    },
+    filteredContacts() {
+      const contactsList = [];
+
+      this.contacts.filter((contact) => {
+        if (contact.name.toLowerCase().indexOf(this.filteredQuery.toLowerCase()) != -1) {
+          contactsList.push(contact);
+        }
+      });
+      return (this.contacts = contactsList);
     },
   },
 });
